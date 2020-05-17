6058b5148a6d127c2918e82f8d3fc42237abebbf
Ryan Shirley <ryanjshirley@gmail.com>
2020-05-17 14:35:46 +0100

    Get metadata from video file

--- modules/Utility.js
+++ modules/Utility.js
@@ -1,26 +1,54 @@
 // Configure Env
-const dotenv = require('dotenv')
+const dotenv = require("dotenv")
 dotenv.config()
 
 // Imports
-const ping = require('ping')
+const ping = require("ping")
+const ffmpeg = require("ffmpeg")
 
 /**
  * checkIfHome() returns status if home is occupied
  * based on the ping status of mobile phones
  */
 export async function checkIfHome() {
-    let hosts = process.env.FAMILY_PHONE_IP_ADDRESSES.split(' ')
+    let hosts = process.env.FAMILY_PHONE_IP_ADDRESSES.split(" ")
 
     // Loop through all hosts to check if alive
     for (let host of hosts) {
         let res = await ping.promise.probe(host)
-        
+
         // If host is alive reutrn
-        if(res.alive) {
+        if (res.alive) {
             return true
-        }        
+        }
     }
 
     return false
+}
+
+/**
+ * getMetaFromVideo() returns metadata for file
+ * based on the passed in video file
+ */
+export function getMetaFromVideo(pathToFile) {
+    try {
+        var process = new ffmpeg("./data/" + pathToFile)
+        return process.then(
+            (video) => {
+                // Video metadata
+                // console.log(video.metadata)
+                // FFmpeg configuration
+                // console.log(video.info_configuration)
+
+                return video.metadata
+            },
+            (err) => {
+                console.log("Error: " + err)
+                return err
+            }
+        )
+    } catch (e) {
+        console.log(e.code)
+        console.log(e.msg)
+    }
 }
--- routes/videos.js
+++ routes/videos.js
@@ -4,6 +4,7 @@ var router = express.Router()
 
 // Queue
 import imageQueue from "../modules/Queue"
+import { getMetaFromVideo } from "../modules/Utility"
 
 /**
  * route('/').get() add test job to queue
@@ -13,6 +14,19 @@ router.route("/").get((req, res) => {
     imageQueue.add({ pathToFile: "images/test.jpg" })
 
     return res.send("Hello World!")
+})
+
+/**
+ * route('/meta').get() return meta from video file
+ */
+router.route("/meta").get(async (req, res) => {
+    let metadata = await getMetaFromVideo("videos/test.mp4")
+
+    return res.send({
+        fps: metadata.video.fps,
+        duration: metadata.duration.seconds,
+        resolution: metadata.video.resolution,
+    })
 })
 
 export default router
