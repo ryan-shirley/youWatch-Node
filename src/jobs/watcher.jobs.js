e43bd5d75fd8fa3941394d8db36db76286fbd567
Ryan Shirley <ryanjshirley@gmail.com>
2020-05-20 12:05:51 +0100

    Fix issue with ffmpeg and spaces in directory path

--- src/jobs/watcher.jobs.js
+++ src/jobs/watcher.jobs.js
@@ -1,19 +1,32 @@
 import Logger from "../loaders/logger"
 import chokidar from "chokidar"
+import moveFile from "move-file"
 
 // Services
 import { addVideoToQueue } from "../services/video.service"
 
 export default async () => {
     // Initialize watcher.
-    const watcher = chokidar.watch("./data/videos/*.mp4", {
-        ignored: /(^|[\/\\])\../, // ignore dotfiles
-        ignoreInitial: true,
-        persistent: true,
-    })
+    const watcher = chokidar.watch(
+        "./data/videos/Reolink Client-Record/*/*.mp4",
+        {
+            ignored: /(^|[\/\\])\../, // ignore dotfiles
+            ignoreInitial: true,
+            persistent: true,
+            awaitWriteFinish: true,
+        }
+    )
 
     // Add event listeners.
-    watcher.on("add", (path, stats) => addVideoToQueue(path))
+    watcher.on("add", async (path, stats) => {
+        const filePathDetails = path.split("/"),
+            fileName = filePathDetails.slice(-1)
 
+        // Move file to analysis folder
+        let newDestination = `data/videos/analysis/${fileName}`
+        await moveFile(path, newDestination)
+
+        return addVideoToQueue(newDestination)
+    })
     Logger.info("Watching folders for new video clips")
 }
